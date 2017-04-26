0af651d874c70bce8ee0ed52585236dfad300d51
John Doe <h.perian@gmail.com>
2017-04-26 10:09:18 +0300

    Now shows date of creation instead of age in digest and queue screens.

--- Custom/Kernel/Output/HTML/Layout.pm
+++ Custom/Kernel/Output/HTML/Layout.pm
@@ -1969,7 +1969,7 @@ sub CustomerAgeInHours {
     return $AgeStrg;
 }
 
-sub CustomerAge {
+sub CustomerAgeOld {
     my ( $Self, %Param ) = @_;
 
     my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
@@ -2009,6 +2009,27 @@ sub CustomerAge {
         $AgeStrg .= int( ( $Age / 60 ) % 60 ) . ' ';
         $AgeStrg .= $Self->{LanguageObject}->Translate($MinuteDsc);
     }
+    return $AgeStrg;
+}
+
+sub CustomerAge {
+    my ( $Self, %Param ) = @_;
+
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $TimeObject = $Kernel::OM->Get('Kernel::System::Time');
+
+    my $Age = defined( $Param{Age} ) ? $Param{Age} : return;
+    my $AgeStrg = '';
+
+    my ($s, $m, $h, $D, $M, $Y) = localtime($TimeObject->SystemTime() - $Age);
+    $s = sprintf("%02d", $s);
+    $m = sprintf("%02d", $m);
+    $h = sprintf("%02d", $h);
+    $D = sprintf("%02d", $D);
+    $M = sprintf("%02d", $M+1);
+    $Y = $Y+1900;
+    $AgeStrg = "$D.$M.$Y $h:$m:$s";
+
     return $AgeStrg;
 }
 
